c1e0d93200a5e2d5bf1cd045f65ab15d36cf4ba0
eslam-99 <em2000999@gmail.com>
2020-02-28 18:55:05 +0200

    eslam mohamed 20170049 SummationSeries

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 			new SimpleSubscriber(),
 			new ReallySimpleSubscriber(),
 			new Fibonacci(),
+			new SummationSeries()
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
