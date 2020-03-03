64ac616b44050f493776c90127e60868a6c03e39
mina faried <36136028+minafaried@users.noreply.github.com>
2020-03-03 19:43:04 +0200

    fix menu

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,33 @@ public class Main {
 			new CircleVolume(),
 			new SphereArea(),
 			new Fibonacci(),
-			new PowerN()
+			new PowerN(),
+			new MultiplicationSeries(),
+			new CircleArea()
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
-		for (ISubscriber sub : subscribers) {
-			mathTopic.addSubscriber(sub);
-		}
+		System.out.println("Enter the function you want to apply");
+		System.out.println("1- SummationSeries");
+		System.out.println("2- CircleVolume");
+		System.out.println("3- SphereArea");
+		System.out.println("4- Fibonacci");
+		System.out.println("5- PowerN");
+		System.out.println("6- MultiplicationSeries");
+		System.out.println("7- CircleArea");
 		Scanner sc = new Scanner(System.in);
+		int ch = sc.nextInt();
+		ISubscriber choice;
+		switch (ch){
+			case 1:choice = new SummationSeries();break;
+			case 2:choice = new CircleVolume();break;
+			case 3:choice = new SphereArea();break;
+			case 4:choice = new Fibonacci();break;
+			case 5:choice = new PowerN();break;
+			case 6:choice = new MultiplicationSeries();break;
+			default:choice = new CircleArea();
+		}
+		mathTopic.addSubscriber(choice);
 		String input = sc.next();
 		mathTopic.dispatchEvent(input);
 	}
