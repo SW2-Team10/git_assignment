38437eb2e7c79612366fdc89789ddb24913464cb
Abdallah Naguib <abdonaguib99@gmail.com>
2020-03-03 21:12:02 +0200

    added all option

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 
 public class Main {
 	private static ISubscriber subscribers [] = {
-			new SimpleSubscriber(),
-			new ReallySimpleSubscriber(),
 			new SummationSeries(),
 			new CircleVolume(),
 			new SphereArea(),
 			new Fibonacci(),
 			new PowerN(),
-			new MultiplicationSeries()
+			new MultiplicationSeries(),
+			new CircleArea()
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
@@ -20,18 +19,23 @@ public class Main {
 		System.out.println("4- Fibonacci");
 		System.out.println("5- PowerN");
 		System.out.println("6- MultiplicationSeries");
+		System.out.println("7- CircleArea");
+		System.out.println("8- Apply all");
 		Scanner sc = new Scanner(System.in);
 		int ch = sc.nextInt();
-		ISubscriber choice;
 		switch (ch){
-			case 1:choice = new SummationSeries();break;
-			case 2:choice = new CircleVolume();break;
-			case 3:choice = new SphereArea();break;
-			case 4:choice = new Fibonacci();break;
-			case 5:choice = new PowerN();break;
-			default:choice = new MultiplicationSeries();
+			case 1:mathTopic.addSubscriber(new SummationSeries());break;
+			case 2:mathTopic.addSubscriber(new CircleVolume());break;
+			case 3:mathTopic.addSubscriber(new SphereArea());break;
+			case 4:mathTopic.addSubscriber(new Fibonacci());break;
+			case 5:mathTopic.addSubscriber(new PowerN());break;
+			case 6:mathTopic.addSubscriber(new MultiplicationSeries());break;
+			case 7:mathTopic.addSubscriber(new CircleArea());break;
+			default:
+				for(ISubscriber subscriber:subscribers){
+					mathTopic.addSubscriber(subscriber);
+				}
 		}
-		mathTopic.addSubscriber(choice);
 		String input = sc.next();
 		mathTopic.dispatchEvent(input);
 	}
